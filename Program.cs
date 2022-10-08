426916de661511bfc437f5837ff2ba68155b9823
Pavel <pavel.sharagin.1994@gmail.com>
2022-10-08 12:13:23 +0500

    task50 done

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int GetRandomValue(int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue + 1);
+}
+void FillArray(int[,] arr, int minValue, int maxValue)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = GetRandomValue(minValue,maxValue);
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] InitializateArray(int m, int n)
+{
+    return new int[m,n];
+}
+void FindValueInArray(int[,] arr, int index)
+{
+   int y = index/10;
+   int x = index/100;
+   if (x>arr.GetLength(0) || y>arr.GetLength(1))
+        Console.WriteLine ("Такого элемента нет");
+   else
+        Console.WriteLine (arr[x,y]);
+}
+//КОД ОСНОВНОЙ ПРОГРАММЫ
+int m = GetNumberFromConsole("Введите размерность массива m");
+int n = GetNumberFromConsole("Введите размерность массива n");
+int[,] array = InitializateArray(m,n);
+int minValue = GetNumberFromConsole("Введите минимальное число, допустимое в массиве");
+int maxValue = GetNumberFromConsole("Введите максимальное число, допустимое в массиве");
+FillArray(array,minValue,maxValue);
+PrintArray(array);
+int index = GetNumberFromConsole("Введите индекс без запятых и пробелов");
+FindValueInArray(array,index);
