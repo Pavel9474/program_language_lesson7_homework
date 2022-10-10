39e55557bb7ffb549ca03edc9ed87e62450e5d39
pavel.sharagin.1994@gmail.com <pavel.sharagin.1994@gmail.com>
2022-10-10 11:35:29 +0500

    addtsk2 done

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// . Напишите программу для умножения массива различных размерностей на число.
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int GetRandomValue(int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue + 1);
+}
+void FillArray(int[,] arr, int minValue, int maxValue)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = GetRandomValue(minValue,maxValue);
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] InitializateArray(int m, int n)
+{
+    return new int[m,n];
+}
+int[,] MultiplyMatrixByNumber(int[,] arr, int k)
+{
+    int[,] MultMatrix=new int[arr.GetLength(0),arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            MultMatrix[i,j]=arr[i,j]*k;
+        }
+    }
+    return MultMatrix;
+}
+//КОД ОСНОВНОЙ ПРОГРАММЫ
+int m = GetNumberFromConsole("Введите размерность массива m");
+int n = GetNumberFromConsole("Введите размерность массива n");
+int[,] array = InitializateArray(m,n);
+int minValue = GetNumberFromConsole("Введите минимальное число, допустимое в массиве");
+int maxValue = GetNumberFromConsole("Введите максимальное число, допустимое в массиве");
+FillArray(array,minValue,maxValue);
+PrintArray(array);
+int k = GetNumberFromConsole("Введите число на которое нужно умножить матрицу");
+PrintArray(MultiplyMatrixByNumber(array,k));
